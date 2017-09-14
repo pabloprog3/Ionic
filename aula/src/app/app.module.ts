0951e3913c4f21fb65132426423192c62fb8f4fa
pablo <pablo_h_n@yahoo.com.ar>
2017-09-13 22:07:35 -0300

    subido app aula con login

---
+++ aula/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+
+//mis importaciones
+import { Firebase } from '@ionic-native/firebase';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage
+  ],
+  providers: [
+    StatusBar,
+    Firebase,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
---
+++ aula/src/pages/home/home.ts
@@ -0,0 +1,69 @@
+import { Component } from '@angular/core';
+import { NavController, Platform } from 'ionic-angular';
+import { Login } from '../../clases/login';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  private usuario: Login;
+  private nombre: string;
+  private passw: number;
+  private errCred: boolean;
+
+  constructor(public navCtrl: NavController, public platform:Platform) {
+    this.nombre = "";
+    this.passw = null;
+    this.errCred = false;
+    this.passw = null;
+   }
+
+  private login():void{
+    this.usuario = new Login();
+    this.usuario.setNombre(this.nombre);
+    this.usuario.setClave(this.passw);
+    //console.log('usuario: ', this.usuario);
+    if (this.usuario.getNombre() == "" || this.usuario.getClave() == null) {
+      this.errCred = true;
+    }else{
+      this.errCred = false;
+    }
+  }
+
+  private writePassw():void{
+    switch (this.nombre) {
+      case "admin":
+        this.passw = 11;
+        this.errCred = false;
+      break;
+      case "invitado":
+        this.passw = 22;
+        this.errCred = false;
+      break;
+      case "usuario":
+        this.passw = 33;
+        this.errCred = false;
+      break;
+      case "j1":
+        this.passw = 44;
+        this.errCred = false;
+      break;
+      case "j2":
+        this.passw = 55;
+        this.errCred = false;
+      break;
+
+      default:
+      break;
+    }
+  }
+
+  private salir():void{
+
+    this.platform.exitApp();
+  }
+
+
+}
